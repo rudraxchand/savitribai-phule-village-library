d2a3c104cb4c93a1a0adc056741549c38cf18088
rudraxchand <rudrax.archives@gmail.com>
2025-12-17 08:21:17 -0700

    Fix vite.config.ts syntax error

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-      base: '/savitribai-phule-village-library/',
     return {
+            base: '/savitribai-phule-village-library/',
       server: {
         port: 3000,
         host: '0.0.0.0',
